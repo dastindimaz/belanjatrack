f547e8405d7953b65e178ddd387511f2e82929f4
dastindimaz <67066316+dastindimaz@users.noreply.github.com>
2026-07-01 17:04:06 +0700

    V2

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "belanjatrack-pwa-v2";
+const APP_SHELL = [
+  "./",
+  "./xlsx.full.min.js",
+  "./chart.umd.min.js",
+  "./manifest.webmanifest",
+  "./pwa-icon-192.png",
+  "./pwa-icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(async (cache) => {
+      await Promise.allSettled(APP_SHELL.map((asset) => cache.add(asset)));
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const network = fetch(event.request)
+        .then((response) => {
+          if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          }
+          return response;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
